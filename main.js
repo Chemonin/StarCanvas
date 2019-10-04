8bdc159b59379a7fb3bab6dc73fdcb38e75abdc8
Chemonin <iachemonin@gmail.com>
2019-10-05 02:16:53 +0300

    закончена реализация

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 const canvas = document.getElementById(`stars-container`);
 const starColor = document.querySelector(`#color`);
 const ctx = canvas.getContext(`2d`);
+const starColorCtx = starColor.getContext(`2d`);
+starColorCtx.fillStyle = `#ffffff`;
+starColorCtx.fillRect(0, 0, starColor.width, starColor.height);
+ctx.fillStyle = `#ffffff`;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 const colors = [`rgb(255, 0, 0)`, `rgb(0, 0, 255)`, `rgb(0, 255, 0)`, `rgb(255, 255, 0)`, `rgb(0, 0, 0)`];
-
-const star = new Path2D();
-star.moveTo(55, 10);
-star.lineTo(25, 104);
-star.lineTo(100, 44);
-star.lineTo(10, 44);
-star.lineTo(85, 104);
-star.lineTo(55, 10);
 
 colors.forEach((color, index) => {
   let x = 0;
   let y = 0;
   if (index < 3) {
-    x = index;
+    x = index * 150;
   } else {
-    x = index - 3;
+    x = (index - 3) * 150;
     y = 150;
   }
-  ctx.save();
   ctx.fillStyle = color;
-  ctx.translate(x * 150, y);
-  ctx.fill(star);
-  ctx.restore();
+  ctx.beginPath();
+  ctx.moveTo(x + 55, y + 10);
+  ctx.lineTo(x + 25, y + 104);
+  ctx.lineTo(x + 100, y + 44);
+  ctx.lineTo(x + 10, y + 44);
+  ctx.lineTo(x + 85, y + 104);
+  ctx.fill();
 });
 
 const onCanvasMousedown = function (evt) {
   const pixel = ctx.getImageData(evt.layerX, evt.layerY, 1, 1);
   const colorData = pixel.data;
-  console.log(colorData);
-  starColor.style.backgroundColor = `rgb(${colorData[0]}, ${colorData[1]}, ${colorData[2]})`;
+  starColorCtx.clearRect(0, 0, starColor.width, starColor.height);
+  starColorCtx.fillStyle = `rgb(${colorData[0]}, ${colorData[1]}, ${colorData[2]})`;
+  starColorCtx.fillRect(0, 0, starColor.width, starColor.height);
 }
 
 canvas.addEventListener(`mousedown`, onCanvasMousedown);
