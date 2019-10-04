db287653f5dfed470a29d3abc428d9da4345f2dc
Chemonin <iachemonin@gmail.com>
2019-10-04 23:57:25 +0300

    added element

---
+++ main.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById(`stars-container`);
+const ctx = canvas.getContext(`2d`);
+const colors = [`rgb(255, 0, 0)`, `rgb(0, 0, 255)`, `rgb(0, 255, 0)`, `rgb(255, 255, 0)`, `rgb(0, 0, 0)`];
+
+const star = new Path2D();
+star.moveTo(55, 10);
+star.lineTo(25, 104);
+star.lineTo(100, 44);
+star.lineTo(10, 44);
+star.lineTo(85, 104);
+star.lineTo(55, 10);
+
+colors.forEach((color, index) => {
+  let x = 0;
+  let y = 0;
+  if (index < 3) {
+    x = index;
+  } else {
+    x = index - 3;
+    y = 150;
+  }
+  ctx.save();
+  ctx.fillStyle = color;
+  ctx.translate(x * 150, y);
+  ctx.fill(star);
+  ctx.restore();
+})
