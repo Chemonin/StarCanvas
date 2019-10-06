e3d28ec048df313944669bb4a148024d259a900a
Chemonin <iachemonin@gmail.com>
2019-10-06 23:33:58 +0300

    added new style

--- main.js
+++ main.js
@@ -6,26 +6,34 @@ starColorCtx.fillStyle = `#ffffff`;
 starColorCtx.fillRect(0, 0, starColor.width, starColor.height);
 ctx.fillStyle = `#ffffff`;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-const colors = [`rgb(255, 0, 0)`, `rgb(0, 0, 255)`, `rgb(0, 255, 0)`, `rgb(255, 255, 0)`, `rgb(0, 0, 0)`];
+const colors = [`rgb(255, 0, 0)`, `rgb(0, 0, 255)`, `rgb(0, 255, 0)`,
+`rgb(255, 255, 0)`, `rgb(0, 0, 0)`,];
 
-colors.forEach((color, index) => {
-  let x = 0;
-  let y = 0;
-  if (index < 3) {
-    x = index * 150;
-  } else {
-    x = (index - 3) * 150;
-    y = 150;
-  }
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(x + 55, y + 10);
-  ctx.lineTo(x + 25, y + 104);
-  ctx.lineTo(x + 100, y + 44);
-  ctx.lineTo(x + 10, y + 44);
-  ctx.lineTo(x + 85, y + 104);
-  ctx.fill();
-});
+const renderStars = function () {
+  let startCordX = 0;
+  let startCordY = 0;
+  colors.forEach((color, index) => {
+
+    if (startCordX >= canvas.width) {
+      startCordX = 0;
+      startCordY += 200;
+    }
+    if (startCordY >= canvas.height) {
+      return;
+    }
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(startCordX + 83, startCordY + 15);
+    ctx.lineTo(startCordX + 38, startCordY + 156);
+    ctx.lineTo(startCordX + 150, startCordY + 66);
+    ctx.lineTo(startCordX + 15, startCordY + 66);
+    ctx.lineTo(startCordX + 126, startCordY + 156);
+    ctx.fill();
+    startCordX += 200;
+  });
+};
+
+renderStars();
 
 const onCanvasMousedown = function (evt) {
   const pixel = ctx.getImageData(evt.layerX, evt.layerY, 1, 1);
@@ -33,6 +41,7 @@ const onCanvasMousedown = function (evt) {
   starColorCtx.clearRect(0, 0, starColor.width, starColor.height);
   starColorCtx.fillStyle = `rgb(${colorData[0]}, ${colorData[1]}, ${colorData[2]})`;
   starColorCtx.fillRect(0, 0, starColor.width, starColor.height);
+  starColor.style.boxShadow = `0 0 5px 10px rgb(${colorData[0]}, ${colorData[1]}, ${colorData[2]})`;
 }
 
 canvas.addEventListener(`mousedown`, onCanvasMousedown);
